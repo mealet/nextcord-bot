242049e26b822917bbbd64e3d833a38068ad3285
jasonragonter <jasonragonter@gmail.com>
2024-01-25 12:46:18 +0300

    Update 1.4 Added roles by dropdown customization

--- cogs/roles.py
+++ cogs/roles.py
@@ -10,25 +10,31 @@ class Roles(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
+        roles_description = ""
+        for i in config.roles:
+            roles_description = roles_description + f"\n<@&{config.roles[i][0]}> - {config.roles[i][1]}."
+
+        roles_description = roles_description + "\n\nЧтобы удалить роль выберите её повторно."
         _guild = self.bot.get_guild(config.guild_id)
         roles_channel = nextcord.utils.get(_guild.channels, id=config.roles_channel_id)
         roles_embed = nextcord.Embed(title="Выбери роль")
         roles_embed.add_field(name="Список доступных ролей",
-                              value="<@&1183105327353311302> - Первыми получают уведомления о новостях.\n<@&1183105429027434527> - Участвуют в серверных мероприятиях\n<@&1183105489207304202> - Ищут друзей по играм и игровым тематикам.\n\nЧтобы удалить роль выберите её повторно")
+                              value=roles_description)
         roles_embed.set_image(url="https://media.giphy.com/media/5tiNlHkA1WdUh3jRDW/giphy.gif")
 
         async def dropdown_callback(inter: nextcord.Interaction):
             for v in dropdown.values:
-                current_role = nextcord.utils.get(inter.guild.roles, id=config.roles[v])
+                current_role = nextcord.utils.get(inter.guild.roles, id=config.roles[v][0])
                 if current_role in inter.user.roles:
                     await inter.user.remove_roles(current_role)
                 else:
                     await inter.user.add_roles(current_role)
 
-        role1 = nextcord.SelectOption(label="Игры", value="games", emoji="😎")
-        role2 = nextcord.SelectOption(label="Мероприятия", value="events", emoji="🍺")
-        role3 = nextcord.SelectOption(label="Новости", value="news", emoji="👀")
-        dropdown = nextcord.ui.Select(placeholder="Выбери роль", options=[role1, role2, role3], max_values=1)
+        options_list = []
+        for x in config.roles:
+            options_list.append(nextcord.SelectOption(label=config.roles[x][2], value=x, emoji=config.roles[x][3]))
+
+        dropdown = nextcord.ui.Select(placeholder="Выбери роль", options=options_list, max_values=1)
         dropdown.callback = dropdown_callback
 
         _view = nextcord.ui.View(timeout=0)
