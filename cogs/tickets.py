195c6fe93985fc0b01f5f73ed97226fa5de935ef
mealet <jasonragonter@gmail.com>
2024-02-17 20:20:53 +0300

    Added tickets transcriptions

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -6,6 +6,7 @@ import config
 from cogs import logs
 import colorama
 from colorama import Back, Fore, Style
+import os
 
 # Note for me :)
 # datetime.now().strftime('%d.%m.%Y %H:%M')
@@ -63,12 +64,42 @@ class Tickets(commands.Cog):
                 close_embed = nextcord.Embed(colour=nextcord.Colour.red(), timestamp=datetime.now())
                 close_embed.add_field(name=f"{inter.user.name} закрыл тикет", value=f"Закрыл пользователь: {inter.user.mention}\nНазвание канала: {ticket_channel.name}\nСоздатель тикета: {roles_mention_str.split(' ')[0]}")
                 # name=f"{inter.user.name} closed ticket", value=f"User closed: {inter.user.mention}\nChannel name: {ticket_channel.name}\nTicket author: {roles_mention_str.split(' ')[0]}"
-                await ticket_channel.send(embed=close_embed)
+                
+
+
+                # saving history in html file
+
+                ts_file = open(f"temp/{ticket_channel.name}.html", "a")
+
+                ts_file.write(f'<h1 style="font-family: Calibri">Ticket "{ticket_channel.name}"</h1>\n')
+
+                async for message in ticket_channel.history(oldest_first=True):
+                    ts_file.write(f'<nav style="font-family: Calibri"><b>{message.author}:</b> {message.content}</nav>\n')
+                
+                ts_file.close()
+
+
+                # sending embed with transcription file
+
+                await ticket_channel.send(embed=close_embed, file=nextcord.File(f"temp/{ticket_channel.name}.html", force_close=True, filename="ticket.html"))
                 await inter.response.send_message("Вы успешно закрыли тикет!", ephemeral=True) # You successfuly closed ticket!
-                await asyncio.sleep(1.5)
+
+                # sending transcription file to ticket's author
+
+                ticket_author = nextcord.utils.get(guild.members, id=int(roles_mention_str.split(' ')[0].replace("<", "").replace("@", "").replace(">", "")))
+                to_author_embed = nextcord.Embed()
+                to_author_embed.add_field(name="Транскрипция тикета", value="Спасибо что воспользовались нашими тикетами! К сообщению прикреплён файл с транскрипцией.")
+                to_author_embed.add_field(name="Зачем мне файл транскрипции?", value="Вы можете использовать его для подачи жалобы")
+
+                await ticket_author.send(embed=to_author_embed, file=nextcord.File(f"temp/{ticket_channel.name}.html", force_close=True, filename="ticket.html"))
+
+                os.remove(f"temp/{ticket_channel.name}.html")
+
+                await asyncio.sleep(0.5)
+
+                # closing ticket
 
                 closed_tickets_category = nextcord.utils.get(guild.categories, id=config.closed_tickets_category)
-                ticket_author = nextcord.utils.get(guild.members, id=int(roles_mention_str.split(' ')[0].replace("<", "").replace("@", "").replace(">", "")))
 
                 await ticket_channel.set_permissions(ticket_author, read_messages=False, send_messages=False)
                 await ticket_channel.set_permissions(inter.user, read_messages=False)
